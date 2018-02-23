0aefee97d30ac230412e11409e480c8624c4feae
dansmog <juwonanthony@gmail.com>
2018-02-23 04:11:08 +0100

    structure for sidebar

---
+++ src/presentational/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Sidebar extends PureComponent {
+    render(){
+        return (
+            <div className="sidebar">
+                <p>This is the sidebar</p>
+            </div>
+        )
+    }
+}
+
+export default Sidebar
